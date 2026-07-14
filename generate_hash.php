3b008cf6fca25c249a292c3d915edf0fcd50707b
Hastsya Angeline <hastsyaangeline@itbss.ac.id>
2026-07-14 20:03:39 +0700

    Update terbaru

--- generate_hash.php
+++ generate_hash.php
@@ -1,20 +1,4 @@
-<?php
 
-$password = 'mingguceriaanak';   // ← ganti sesuai password yang diinginkan
-$hash = kata_sandi_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
-
-echo '<pre>';
-echo "Password   : $password\n";
-echo "Hash (bcrypt): $hash\n\n";
-echo "Jalankan SQL berikut di phpMyAdmin:\n\n";
-echo "UPDATE pengguna SET kata_sandi_hash = '$hash' WHERE nama_pengguna = 'admin';";
-echo '</pre>';
-
-$password = 'smgrj01'; // Password yang diinginkan
-
-$hash = kata_sandi_hash($password, PASSWORD_BCRYPT, [
-    'cost' => 12
-]);
 
 echo '<pre>';
 echo "Password : $password\n";
